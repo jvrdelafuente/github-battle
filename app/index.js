426ffe1a6ca087ea2f01511c3d634c4d5c54a133
javier_datahug <javier.delafuente@datahug.com>
2020-04-09 15:34:20 +0100

    1. First Component

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                Hello world!!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    // React Element
+    <App />,
+    // Where to render the Element to
+    document.getElementById('app')
+)
