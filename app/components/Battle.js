4a120ff4dfe85bc281d6601e85eaf964778f8f57
javier_datahug <javier.delafuente@datahug.com>
2020-04-11 15:54:29 +0100

    10. PlayerInput

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa'
+import PropsTypes from 'prop-types'
 
 function Intructions(){
     return (
@@ -25,12 +26,70 @@ function Intructions(){
     )
 }
 
+class PlayerInput extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            userName: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleSubmit(event){
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.userName)
+    }
+    handleChange(event){
+        this.setState({
+            userName: event.target.value
+        })
+    }
+    render(){
+        return(
+            <form className="colunm player" onSubmit={this.handleSubmit}>
+                <label htmlFor='username' className='player-label'>
+                    {this.props.label}
+                </label>
+                <div className="row players-inputs">
+                    <input 
+                        type='text'
+                        id='username'
+                        className='input-light'
+                        placeholder='github username'
+                        autoComplete='off'
+                        value={this.state.userName}
+                        onChange={this.handleChange}
+                    />
+
+                    <button className="btn dark-btn" type='submit' disabled={!this.state.userName}>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    onSubmit: PropsTypes.func.isRequired,
+    label: PropsTypes.string.isRequired
+}
+
+
 export default class Battle extends React.Component{
 
+    onSubmit(username){
+        console.log('change ' + username);
+    }
     render(){
         return (
             <React.Fragment>
                 <Intructions/>
+                <PlayerInput label="username" onSubmit={this.onSubmit}/>
             </React.Fragment>
         )
     }
