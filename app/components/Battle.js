eb2cdedabd57d4e310a17438b7c7ac721a1d8918
Javier De La Fuente Sales <javierdelafuentesales@Javiers-MacBook-Air.local>
2020-04-16 21:41:18 +0100

    18. Profile List

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -139,7 +139,8 @@ export default class Battle extends React.Component{
         const { playerOne, playerTwo, battle } = this.state
 
         if (battle === true) {
-            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+            return <Results 
+                playerOne={playerOne} playerTwo={playerTwo} />
           }
 
         return (
